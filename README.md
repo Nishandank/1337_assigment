f4019b56713ac4b6aa27762df38bcc0e8daca1ec
Nishandan Kulanthaivetpillai <nishandan.kulanthaivetpillai@lfv.se>
2022-09-28 17:17:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Guide
+
+## Installation
+#### Requirements
+* Python 3.6 or greater
+
+
+#### Run script
+* pip install -r requirements.txt
+* pip install .
+* python script.py
---
+++ setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup, find_packages
+
+
+setup(name='WebScraper',
+      version='1.0',
+      description='Scrape and download',
+      packages=find_packages(),
+     )
